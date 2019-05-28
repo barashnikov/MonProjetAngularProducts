085e4196f3cebd8297289bfeca31e6b2cfc692dd
Fakiri Mohamed <39508@heb.be>
2019-05-28 15:48:47 +0200

    Commit de la fonctionnalité de routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsComponent } from './components/products/products.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 registerLocaleData(localeFr, 'fr');
+const routingTable : Route[] = [
+  {path : '', redirectTo : 'welcome', pathMatch: 'full'},
+  {path : 'welcome', component : WelcomeComponent},
+  {path : 'products', component : ProductsComponent},
+  {path : '**', component : NotFoundComponent}
+
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routingTable)
   ],
   providers: [],
   bootstrap: [AppComponent]
